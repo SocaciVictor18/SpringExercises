d11542ccc9aeb7c476750f9d2164e38abb78ad7a
Victor Socaci <Victor.Socaci@endava.com>
2025-11-29 19:59:15 +0200

    Using the @Qualifier annotation, I specify the name of the bean I want Spring to inject from the context, and I don’t rely on the identifier of the constructor’s parameter

--- SpringEx/src/main/java/Ex1/Person.java
+++ SpringEx/src/main/java/Ex1/Person.java
@@ -1,6 +1,7 @@
 package Ex1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +11,8 @@ public class Person {
 
     private final Parrot parrot;
 
-    public Person(Parrot parrot2) {
-        this.parrot = parrot2;
+    public Person(@Qualifier("parrot2") Parrot parrot) {
+        this.parrot = parrot;
     }
 
     public String getName() {
