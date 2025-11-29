8a191c968c70bdabfbf357767d63282a07e51ef5
Victor Socaci <Victor.Socaci@endava.com>
2025-11-29 19:52:15 +0200

    Using the @Qualifier annotation

--- SpringEx/src/main/java/Ex1/ProjectConfig.java
+++ SpringEx/src/main/java/Ex1/ProjectConfig.java
@@ -1,5 +1,6 @@
 package Ex1;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -25,12 +26,37 @@ public class ProjectConfig {
     }
 
     @Bean
-    public Person person(Parrot parrot2) {
+    public Person person(
+            @Qualifier("parrot2") Parrot parrot) {
+
         Person p = new Person();
         p.setName("Ella");
-        p.setParrot(parrot2);
+        p.setParrot(parrot);
         return p;
     }
+
+//
+//    @Bean
+//    public Parrot parrot1() {
+//        Parrot p = new Parrot();
+//        p.setName("Koko");
+//        return p;
+//    }
+//
+//    @Bean
+//    public Parrot parrot2() {
+//        Parrot p = new Parrot();
+//        p.setName("Miki");
+//        return p;
+//    }
+//
+//    @Bean
+//    public Person person(Parrot parrot2) {
+//        Person p = new Person();
+//        p.setName("Ella");
+//        p.setParrot(parrot2);
+//        return p;
+//    }
 
 
 //    @Bean
