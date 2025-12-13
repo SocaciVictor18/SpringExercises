abf6cf006d329824534647bac7ee184e4c284a0c
Victor Socaci <Victor.Socaci@endava.com>
2025-12-13 16:39:32 +0200

    Add dependencies for thymeleaf and this is an example of requestParam

---
+++ Chapter7/src/main/java/org/example/chapter7/controller/MainController.java
@@ -0,0 +1,42 @@
+package org.example.chapter7.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+//    @RequestMapping("/home")
+//    public String home() {
+//        return "home.html";
+//    }
+
+//    @RequestMapping("/home")
+//    public String home(Model page){
+//        page.addAttribute("username", "Victor");
+//        page.addAttribute("color", "red");
+//        return "home.html";
+//    }
+
+    //Request parameter
+//
+//    @RequestMapping("/home")
+//    public String home(
+//            @RequestParam String color,
+//            Model page) {
+//        page.addAttribute("username", "Victor");
+//        page.addAttribute("color", color);
+//        return "home.html";
+//    }
+    @RequestMapping("/home")
+    public String home(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String color,
+            Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+}
