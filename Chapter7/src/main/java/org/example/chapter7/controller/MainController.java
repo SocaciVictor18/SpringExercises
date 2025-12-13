f573032cd8b6396e219c13186832b5eace040341
Victor Socaci <Victor.Socaci@endava.com>
2025-12-13 16:53:57 +0200

    Example of pathvartiable

--- Chapter7/src/main/java/org/example/chapter7/controller/MainController.java
+++ Chapter7/src/main/java/org/example/chapter7/controller/MainController.java
@@ -2,6 +2,7 @@ package org.example.chapter7.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,13 +31,25 @@ public class MainController {
 //        page.addAttribute("color", color);
 //        return "home.html";
 //    }
-    @RequestMapping("/home")
+//    @RequestMapping("/home")
+//    public String home(
+//            @RequestParam(required = false) String name,
+//            @RequestParam(required = false) String color,
+//            Model page) {
+//        page.addAttribute("username", name);
+//        page.addAttribute("color", color);
+//        return "home.html";
+//    }
+
+    @RequestMapping("/home/{color}")
     public String home(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String color,
-            Model page) {
-        page.addAttribute("username", name);
+        @PathVariable String color,
+        Model page
+    ){
+        page.addAttribute("username", "Socata1");
         page.addAttribute("color", color);
-        return "home.html";
+        return  "home.html";
     }
+
+
 }
