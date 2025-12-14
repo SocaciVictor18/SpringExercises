7c4874072201ccca74be7b7dedca420d6f0c96f9
Victor Socaci <Victor.Socaci@endava.com>
2025-12-14 19:36:33 +0200

    Writing UnitTests

--- Chapter12/src/test/java/org/example/chapter12/service/TransferServiceUnitTests.java
+++ Chapter12/src/test/java/org/example/chapter12/service/TransferServiceUnitTests.java
@@ -6,6 +6,10 @@ import org.example.chapter12.repository.AccountRepo;
 import org.example.chapter12.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -14,17 +18,17 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class TransferServiceUnitTests {
 
-    @Test
-    @DisplayName("Test the amount is transferred " +
-            "from one account to another if no exception occurs.")
-    public void moneyTransferHappyFlow() {
-        AccountRepo accountRepository =
-                mock(AccountRepo.class);
-        TransferService transferService =
-                new TransferService(accountRepository);
+    @Mock
+    private AccountRepo accountRepository;
 
+    @InjectMocks
+    private TransferService transferService;
+
+    @Test
+    public void moneyTransferHappyFlow() {
         Account sender = new Account();
         sender.setId(1);
         sender.setAmount(new BigDecimal(1000));
@@ -34,23 +38,55 @@ public class TransferServiceUnitTests {
         destination.setAmount(new BigDecimal(1000));
 
         given(accountRepository.findById(sender.getId()))
-      .willReturn(Optional.of(sender));
+                .willReturn(Optional.of(sender));
 
         given(accountRepository.findById(destination.getId()))
-      .willReturn(Optional.of(destination));
+                .willReturn(Optional.of(destination));
 
-        transferService.transferMoney(
-                sender.getId(),
-                destination.getId(),
-                new BigDecimal(100)
-        );
+        transferService.transferMoney(1, 2, new BigDecimal(100));
 
         verify(accountRepository)
-      .changeAmount(1, new BigDecimal(900));
+                .changeAmount(1, new BigDecimal(900));
 
         verify(accountRepository)
-      .changeAmount(2, new BigDecimal(1100));
+                .changeAmount(2, new BigDecimal(1100));
     }
+
+//    @Test
+//    @DisplayName("Test the amount is transferred " +
+//            "from one account to another if no exception occurs.")
+//    public void moneyTransferHappyFlow() {
+//        AccountRepo accountRepository =
+//                mock(AccountRepo.class);
+//        TransferService transferService =
+//                new TransferService(accountRepository);
+//
+//        Account sender = new Account();
+//        sender.setId(1);
+//        sender.setAmount(new BigDecimal(1000));
+//
+//        Account destination = new Account();
+//        destination.setId(2);
+//        destination.setAmount(new BigDecimal(1000));
+//
+//        given(accountRepository.findById(sender.getId()))
+//      .willReturn(Optional.of(sender));
+//
+//        given(accountRepository.findById(destination.getId()))
+//      .willReturn(Optional.of(destination));
+//
+//        transferService.transferMoney(
+//                sender.getId(),
+//                destination.getId(),
+//                new BigDecimal(100)
+//        );
+//
+//        verify(accountRepository)
+//      .changeAmount(1, new BigDecimal(900));
+//
+//        verify(accountRepository)
+//      .changeAmount(2, new BigDecimal(1100));
+//    }
 }
 
 
